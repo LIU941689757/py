424db96e60e524e290b8eac7d18bdb7aa42dcb33
LIU. <76978938+LIU941689757@users.noreply.github.com>
2025-02-25 10:35:01 +0000

    bat

---
+++ BAT/WebexAllVersionIUninstall.bat
@@ -0,0 +1,18 @@
+@echo off
+echo 正在查找 Webex 产品代码...
+for /f "tokens=2 delims={}" %%i in ('wmic product where "name like '%%Webex%%'" get IdentifyingNumber /format:list ^| findstr IdentifyingNumber') do (
+    set ProductCode={%%i}
+echo %%i
+)
+
+if not defined ProductCode (
+    echo 未找到 Webex 安装，请检查是否已安装。
+    pause
+    exit /b
+)
+
+echo 正在卸载 Webex，请稍候...
+start /wait msiexec /x %ProductCode% /qn
+
+echo Webex 已卸载完成。
+pause
